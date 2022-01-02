0dfd45d1c7f50fe8f6b7dedd00f5ae88748a34bf
Jackie Dallas <jackie.r.dallas@gmail.com>
2022-01-02 01:52:16 -0600

    started personal website

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(express.static("public"));
+
+
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/main.html");
+})
+
+
+
+app.listen(3000, function () {
+	console.log("Server is running...");
+})
