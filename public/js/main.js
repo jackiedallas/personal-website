99cdcebc287f231f76207ddb71eb8652fee8441a
Jackie Dallas <jackie.r.dallas@gmail.com>
2022-02-09 01:36:10 -0600

    added footer and changed colors

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,36 @@
 $(document).ready(function () {
 	
 	const techSkills = document.querySelectorAll('.tech-skills');
+	const softSkills = document.querySelectorAll('.soft-skills');
 	
-	const observer = new IntersectionObserver(entries => {
-		console.log(entries);
+	// tech observer start
+	const techObserver = new IntersectionObserver(entries => {
+		// console.log(entries);
 		// entries.target.classList.toggle('appear', entries.isIn)
 		entries.forEach(entry => {
-			entry.target.classList.toggle('show', entry.isIntersecting)
+			entry.target.classList.toggle('show-tech', entry.isIntersecting)
 		})
 	},{
-		// threshold: 1
 		rootMargin: '-100px'
 	})
 
 	techSkills.forEach(skill => {
-		observer.observe(skill);
+		techObserver.observe(skill);
 	})
+	// tech observer end
+	
+	// soft observer start
+	const softObserver = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			entry.target.classList.toggle('show-soft', entry.isIntersecting)
+		})
+	},{
+		rootMargin: '-100px'
+	})
+	
+	softSkills.forEach(skill => {
+		softObserver.observe(skill)
+	})
+	// soft observer end
 })
 
