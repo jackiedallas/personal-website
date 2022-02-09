4275906d5c82968fa2a1da09b501171c737cae91
Jackie Dallas <jackie.r.dallas@gmail.com>
2022-02-08 23:57:47 -0600

    added intersection observer to tech skills

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,20 @@
 $(document).ready(function () {
+	
+	const techSkills = document.querySelectorAll('.tech-skills');
+	
+	const observer = new IntersectionObserver(entries => {
+		console.log(entries);
+		// entries.target.classList.toggle('appear', entries.isIn)
+		entries.forEach(entry => {
+			entry.target.classList.toggle('show', entry.isIntersecting)
+		})
+	},{
+		// threshold: 1
+		rootMargin: '-100px'
+	})
 
+	techSkills.forEach(skill => {
+		observer.observe(skill);
+	})
 })
 
